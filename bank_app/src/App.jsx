49f593c89551b69c5cae966e879602283af757fd
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-12-24 09:38:25 +0530

    Navbar added

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import styles from './style'
+import { Navbar,  Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
-               NavBar
+               <Navbar/>
           </div>
        </div>
 
-       <div className={`bg-primary  ${styles.flexStart}`}>
+       <div className={`bg-primary w-full ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-              Hero
+              <Hero/>
           </div>
        </div>
-       <div className={`bg-primary  ${styles.paddingX} ${styles.flexStart}`}>
+       <div className={`bg-primary w-full ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-              Stats
+              <Stats/>
+              <Business/>
+              <Billing />
+              <CardDeal/>
+              <Testimonials/>
+              <Clients/>
+              <CTA/>
+              <Footer/>
           </div>
        </div>
     </div>
