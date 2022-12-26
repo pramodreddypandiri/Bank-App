fe215cb92d9c3de0ad71e0c49fa3d417f258160d
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-12-26 21:42:47 +0530

    website completed

--- bank_app/src/components/Billing.jsx
+++ bank_app/src/components/Billing.jsx
@@ -11,8 +11,8 @@ function Billing() {
       <div className='absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
     </div>
     <div className={layout.sectionInfo}>
-      <h2>
-        Easily control your <br className='sm:block hiddden'/> billing & invoicing
+      <h2 className={styles.heading2}>
+        Easily control your <br className='sm:block hidden'/> billing & invoicing
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Elit enim sed massa etiam. Mauris eu adipisscing ultrices ametodio aenean neque fusce ipsum orci rhoncus aliporttitor integer platea placerat.
--- bank_app/src/components/CardDeal.jsx
+++ bank_app/src/components/CardDeal.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { card } from '../assets' 
+import styles, {layout} from '../style'
+import Button from './Button'
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='sm:block hidden'/> in a few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5 text-white`}>
+        Arcu totor purus in mattis at sed integer facibus. Aliquet quis Aliquet eget mauris tortor.c Aliquet ultirces sc, ameta.
+      </p>
+      <Button styles="mt-10"/>
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]'/>
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+    </div>
+  </section>
+)
 
 export default CardDeal
